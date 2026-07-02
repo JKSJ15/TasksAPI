9a3bf57fa9d0e0c1c11fc690c0ae21c19e227043
Jakson josé <josejakson546@gmail.com>
2026-07-02 16:06:27 -0300

    Adjust the formatting of the exception handler

--- src/main/java/com/spring/taskAPI/exception/Handler/GlobalExceptionsHandler.java
+++ src/main/java/com/spring/taskAPI/exception/Handler/GlobalExceptionsHandler.java
@@ -29,12 +29,14 @@ public class GlobalExceptionsHandler {
 				.withErro(HttpStatus.NOT_FOUND).withStatus(HttpStatus.NOT_FOUND.value()).build();
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
 	}
+
 	@ExceptionHandler(value = InvalidRefreshTokenException.class)
 	public ResponseEntity<BodyExceptions> useInvalidRefreshTokenException(InvalidRefreshTokenException e) {
 		BodyExceptions body = BodyExceptions.builder().withMessage(e.getMessage()).withTimestamp(LocalDateTime.now())
 				.withErro(HttpStatus.UNAUTHORIZED).withStatus(HttpStatus.UNAUTHORIZED.value()).build();
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
 	}
+
 	@ExceptionHandler(value = TokenWasExpiredException.class)
 	public ResponseEntity<BodyExceptions> useTokenWasExpiredException(TokenWasExpiredException e) {
 		BodyExceptions body = BodyExceptions.builder().withMessage(e.getMessage()).withTimestamp(LocalDateTime.now())
